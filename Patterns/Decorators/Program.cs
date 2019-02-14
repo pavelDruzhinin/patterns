63ed6d26b433e40037e451a34d5d13a63bb8f5fa
Pavel Druzhinin <paveldsirius@gmail.com>
2019-02-14 23:41:56 +0300

    before apply decorators

---
+++ Patterns/Decorators/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Decorators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public abstract class Beverage
+    {
+        public abstract string Description { get; }
+        public abstract float Cost();
+
+        protected bool HasMilk()
+        {
+
+        }
+        protected float SetMilk()
+        {
+
+        }
+    }
+
+    public class HouseBlend : Beverage
+    {
+
+    }
+
+    public class DarkRoast : Beverage
+    {
+
+    }
+
+    public class Decaf : Beverage
+    {
+
+    }
+
+    public class Espresso : Beverage
+    {
+
+    }
+}
